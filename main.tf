d3d8b6622650839240871d6bc965356ed20498bb
RJ <rjparaiso369@gmail.com>
2025-03-03 23:46:28 +0000

    feat: Updated EC2 instance creation

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 ## Variables
 
-
 variable "access_key" {
   description = "AWS access key"
   type        = string
@@ -13,25 +12,49 @@ variable "secret_key" {
   sensitive   = true
 }
 
-#####################################
+### 
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-northeast-3"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-resource "aws_instance" "janebot" {
-  ami                     = "ami-0439cd8bc5628c9e8"
-  instance_type           = "t2.micro"
+resource "aws_key_pair" "my_key" {
+  key_name   = "terraform-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "ssh_access" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  ami             = "ami-053e5b2b49d1b2a82"
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.my_key.key_name
+  security_groups = [aws_security_group.ssh_access.name]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.my_ec2.public_ip
 }
